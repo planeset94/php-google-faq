a7a8bfbf06574d681f047be7fffa9b77eaef5f1b
planeset94 <planeset94@gmail.com>
2021-06-23 16:30:01 +0200

    link css file. prb par style: can't set diff prop.

--- index.php
+++ index.php
@@ -2,18 +2,10 @@
 <?php 
 
 include('./asset/php/db.php');
- 
-
-
   
 ?>
 
 </pre>
-
-
-
-
-
 
 
 <!DOCTYPE html>
@@ -22,24 +14,42 @@ include('./asset/php/db.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./asset/css/style.css">
     <title>Google answer</title>
 </head>
 <body>
+
+
+<div class="container">
+
+<header>
+    <div class="logo">
+        <img src="./asset/img/googlepng.png" alt="google logo">
+    </div>
+    <span>Privacy & Termini </span>
+</header>
+<div class="navbar">
+   
+ </div>
+
+
+
+
+
     
-<div class="dataset">
-    <?php foreach ($dataset as $data_el) {
-        foreach ($data_el as $key => $value) { ?>
+    <div class="dataset">
+        <?php foreach ($dataset as $data_el) {
+            foreach ($data_el as $key => $value) { ?>
 
-            <p><?=  $key;   ?></p>
-            <p><?=  $value;   ?></p>
+                <p class="domanda"><?=  $data_el[$key];   ?></p>
+                <p class="risposta"><?=  $data_el[$value];   ?></p>
 
-            
+        <?php       
+            }    
+        }
+        ?>
 
-    <?php       
-        }    
-    }
-    ?>
-
+    </div>
 </div>
 </body>
 </html>
