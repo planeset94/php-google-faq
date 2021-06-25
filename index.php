1ed2023dd67aaad688d76b1b351860228e613544
planeset94 <planeset94@gmail.com>
2021-06-25 09:59:15 +0200

    adj underline on hover link. Can't find right font

--- index.php
+++ index.php
@@ -11,6 +11,15 @@ include('./asset/php/db.php');
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link
+        href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;500;600;700&family=Roboto+Condensed:wght@300;400;700&family=Ubuntu:wght@300;400;500&display=swap"
+        rel="stylesheet">
     <link rel="stylesheet" href="./asset/css/style.css">
     <title>Google answer</title>
 </head>
